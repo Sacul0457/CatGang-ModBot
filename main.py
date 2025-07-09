0a3541947bd06bce4c773331ac423ecc647ac43b
Sacul <lucastanhongyi@gmail.com>
2025-07-09 17:51:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,21 @@ import time
 import random
 
 
+TOKEN = os.getenv("TEST_TOKEN")
 
-TOKEN = os.getenv("TOKEN")
-
-cogs = ("automod", "mod", "utilities")
+cogs = ("mod", "logs", "automod")
 
 async def main():
     async with asqlite.connect("mod.db") as conn:
         await conn.execute('''CREATE TABLE IF NOT EXISTS moddb(
-                           case_id TEXT  PRIMARY KEY,
+                           case_id TEXT PRIMARY KEY, 
                            user_id INTEGER,
                            action TEXT,
-                           mod_id INTEGER) ''')
+                           mod_id INTEGER,
+                           time FLOAT) ''')
         await conn.execute('''CREATE INDEX IF NOT EXISTS user_id_index on moddb(user_id)''')
         await conn.execute('''CREATE INDEX IF NOT EXISTS mod_id_index on moddb(mod_id)''')
+        await conn.execute('''CREATE INDEX IF NOT EXISTS time_index on moddb(time)''')
 
         await conn.execute('''CREATE TABLE IF NOT EXISTS tempbandb(
                            user_id INTEGER PRIMARY KEY,
@@ -40,17 +41,27 @@ class ModBot(commands.Bot):
         intents.members = True
         intents.guilds = True
         intents.guild_messages = True
-        intents.moderation = True
+        #intents.moderation = True
         intents.auto_moderation = True
-        super().__init__(command_prefix=commands.when_mentioned_or("!"), intents=intents, help_command=None)
+        intents.emojis_and_stickers = True
+        super().__init__(command_prefix=commands.when_mentioned_or("-"), intents=intents, help_command=None)
+        self.spam_limit = commands.CooldownMapping.from_cooldown(5, 2.5, type=commands.BucketType.member)
     async def setup_hook(self):
-        asyncio.get_event_loop().set_debug(True)
         self.mod_pool = await asqlite.create_pool("mod.db", size=4)
-        await self.load_extension("mod")
+        for cog in cogs:
+            try:
+                await self.load_extension(cog)
+                print(f"Loaded: {cog}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+        asyncio.get_event_loop().set_debug(True)
+
+    
     
     async def close(self):
         await self.mod_pool.close()
         await super().close()
 bot = ModBot()
+
 
 bot.run(TOKEN)
