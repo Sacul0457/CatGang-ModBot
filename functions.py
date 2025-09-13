78b72d84006a74a4b63cdfe067b7b65f790aeaee
Sacul <lucastanhongyi@gmail.com>
2025-09-13 14:32:39 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,24 +14,29 @@ async def save_to_moddb(bot: ModBot, case_id: str, user_id: int, action: Literal
         await conn.execute('''INSERT INTO moddb (case_id, user_id, action, mod_id, time, log_id) VALUES (?, ?, ?, ?, ?, ?)''',
                            (case_id, user_id, action, mod_id, time, log_id))
         await conn.commit()
+
+
 async def save_to_appealdb(bot: ModBot, thread_id: int, user_id: int, action: str) -> None:
     async with bot.mod_pool.acquire() as conn:
         await conn.execute('''INSERT INTO appealdb (thread_id, user_id, action) VALUES (?, ?, ?)
-                           ON CONFLICT DO UPDATE SET user_id = user_id''',
+                           ON CONFLICT(thread_id) DO UPDATE SET user_id = excluded.user_id, action = excluded.action''',
                            (thread_id, user_id, action))
         await conn.commit()
-        
+
+
 async def delete_from_appealdb(bot: ModBot, thread_id: int) -> None:
     async with bot.mod_pool.acquire() as conn:
         await conn.execute('''DELETE FROM appealdb WHERE thread_id = ?''',
                            (thread_id, ))
         await conn.commit()
 
+
 async def double_query(bot: ModBot, *, query_one: str, value_one: tuple, query_two: str, value_two: tuple) -> None:
     async with bot.mod_pool.acquire() as conn:
         await conn.execute(query_one, value_one)
         await conn.execute(query_two, value_two)
         await conn.commit()
+
 
 async def execute_sql(query: str) -> dict[str, Any]:
     async with asqlite.connect('mod.db') as conn:
@@ -47,6 +52,7 @@ async def execute_sql(query: str) -> dict[str, Any]:
 def convert_to_base64() -> str:
     u = uuid4()
     return base64.urlsafe_b64encode(u.bytes).rstrip(b"=").decode("ascii")
+
 
 def sql_to_dict(sql_results: list[tuple]) -> dict[str, Any]:
     """Formats a sql.Row into dict"""
