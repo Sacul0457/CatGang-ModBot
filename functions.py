cfcbf17f48ac7bb60442092fc1db4f9dfbac21f2
Sacul <lucastanhongyi@gmail.com>
2025-09-13 12:16:44 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Literal, Any
 from uuid import uuid4
 import base64
+import asqlite
 if TYPE_CHECKING:
     from main import ModBot
 
@@ -32,8 +33,38 @@ async def double_query(bot: ModBot, *, query_one: str, value_one: tuple, query_t
         await conn.execute(query_two, value_two)
         await conn.commit()
 
+async def execute_sql(query: str) -> dict[str, Any]:
+    async with asqlite.connect('mod.db') as conn:
+        try:
+            row = await conn.execute(query)
+        except Exception as e:
+            raise e
+        result = await row.fetchall()
+        return sql_to_dict(result)
+
 
 #FUNCTIONS
 def convert_to_base64() -> str:
     u = uuid4()
     return base64.urlsafe_b64encode(u.bytes).rstrip(b"=").decode("ascii")
+
+def sql_to_dict(sql_results: list[tuple]) -> dict[str, Any]:
+    """Formats a sql.Row into dict"""
+
+    possible_queries = ('case_id', 'user_id', 'time', 'action', 'mod_id', 'log_id', 'thread_id') #All the possible queries in all the tables
+    data = {}
+    for row in sql_results: # fetchall() returns a list of tuples, so we loop through the list
+        for query in possible_queries: 
+            try:
+                value = row[query] # Try to fetch the value from the row
+            except IndexError:
+                continue
+            if query in data: # For example 'SELECT user_id FROM epi_users', it will make user_id a list
+                if not isinstance(data[query], list):
+                    data[query] = [data[query]] if query in data else []
+                data[query].append(value)
+            else:
+                data[query] = value
+
+    return data
+                
